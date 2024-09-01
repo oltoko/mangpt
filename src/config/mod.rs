13f4b517de6757d494ee3dbfd15d9b2f38e33cf1
Oliver Koch <github@oltoko.de>
2024-09-01 18:12:31 +0200

    add hint where to create an API Key

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -30,6 +30,7 @@ pub fn load() -> Result<ManGPTConfig, Box<dyn std::error::Error>> {
 fn create_config_file(
     config_file_path: &std::path::PathBuf,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    println!("You can create an OpenAI API-Key in your account https://platform.openai.com/api-keys");
     let api_key = rpassword::prompt_password("Please Enter your OpenAI API-Key: ")?;
 
     let config = ManGPTConfig {
